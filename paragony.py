894515d50dab3743af373765823f03b09590c954
Filipek <filips750@gmail.com>
2021-10-20 10:50:35 +0200

    fixed bug when grosz is less than 10

--- paragony.py
+++ paragony.py
@@ -1,10 +1,10 @@
 
-def get_description(name,price):return f'Price of {name} is {price//100}.{price%100}'
+def get_description(name,price):return f'Price of {name} is {price//100:}.{price%100:02}'
 def print_description(name,price): 
     description = get_description(name,price)
     print (description)
 
 
 
-print_description("banana", 434)
-print_description("banana", 200)
+# print_description("banana", 434)
+print_description("banana", 201)
