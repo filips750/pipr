9e3866de41428aa1bbb96ee19471e8a44eafe1e0
Filipek <filips750@gmail.com>
2021-10-20 10:55:57 +0200

    refactorisation of get_description

--- paragony.py
+++ paragony.py
@@ -1,5 +1,11 @@
+def split_price(price):
+    price_zl = price //100
+    price_gr = price % 100
+    return (price_zl,price_gr)
 
-def get_description(name,price):return f'Price of {name} is {price//100:}.{price%100:02}'
+def get_description(name,price):
+    price_parts = split_price(price)
+    return f'Price of {name} is {price_parts[0]}.{price_parts[1]:02}'
 def print_description(name,price): 
     description = get_description(name,price)
     print (description)
