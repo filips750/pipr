8aadaf2adec8434f2a9edef701f00b651d2f6f61
Filipek <filips750@gmail.com>
2021-10-20 10:42:22 +0200

    Rewrittten print_description() for more clarity

--- paragony.py
+++ paragony.py
@@ -1,5 +1,9 @@
 def get_description(name,price_zl,price_gr):return f'Price of {name} is {price_zl}.{price_gr}'
-def print_description(name,price_zl,price_gr): print(get_description(name,price_zl,price_gr))
-print_description("banana", 1, 42)
+def print_description(name,price_zl,price_gr): 
+    description = get_description(name,price_zl,price_gr)
+    print (description)
 
+
+
+print_description("banana", 1, 42)
 
