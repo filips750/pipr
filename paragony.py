25968b5399b7f43d62263daee18e4a66ce6bdba6
Filipek <filips750@gmail.com>
2021-10-25 13:20:25 +0200

    Enabled linting using flake 8, corrected problems eported by flake8

--- paragony.py
+++ paragony.py
@@ -1,16 +1,18 @@
 def split_price(price):
-    price_zl = price //100
+    price_zl = price // 100
     price_gr = price % 100
-    return (price_zl,price_gr)
+    return (price_zl, price_gr)
 
-def get_description(name,price):
+
+def get_description(name, price):
     price_parts = split_price(price)
     return f'Price of {name} is {price_parts[0]}.{price_parts[1]:02}'
-def print_description(name,price): 
-    description = get_description(name,price)
-    print (description)
 
 
+def print_description(name, price):
+    description = get_description(name, price)
+    print(description)
 
-# print_description("banana", 434)
-print_description("banana", 201)
+
+print_description("banana", 434)
+print_description("Oranges", 201)
